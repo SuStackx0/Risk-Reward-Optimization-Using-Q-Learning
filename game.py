ad397e005e0aa913681602b36c50af11dc3adc41
SuStackx0 <gs703880@gmail.com>
2025-12-15 23:05:48 +0530

    feat game setup done

---
+++ game.py
@@ -0,0 +1,32 @@
+import random
+
+class BettingGame:
+    def __init__(self):
+        self.coins=100
+        self.win_prob=0.6
+        
+        
+    def step(self,bet:int):
+        if bet>self.coins:
+            bet=self.coins
+            
+        win=random.random() < self.win_prob
+        
+        old_coins=self.coins
+        
+        if win:
+            self.coins += bet
+        else:
+            self.coins -= bet
+            
+        reward=self.coins-old_coins
+        
+        return self.coins,reward,win
+    
+    
+game = BettingGame()
+
+coins, reward, win = game.step(1)
+print("coins:",coins)
+print("reward:",reward)
+print("win:",win)
